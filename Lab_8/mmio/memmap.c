ef11d1caf43cb07574a07abc369f98d68c0f2195
Tim Elvart <telvart@ku.edu>
2016-10-26 15:26:55 -0500

    work on lab

--- Lab_8/mmio/memmap.c
+++ Lab_8/mmio/memmap.c
@@ -36,8 +36,8 @@ int main (int argc, char *argv[])
   if (argc != 3)
     err_quit ("usage: memmap <fromfile> <tofile>");
 
-  /* 
-   * open the input file 
+  /*
+   * open the input file
    */
   if ((fdin = open (argv[1], O_RDONLY)) < 0) {
     sprintf(buf, "can't open %s for reading", argv[1]);
@@ -45,8 +45,8 @@ int main (int argc, char *argv[])
     exit(errno);
   }
 
-  /* 
-   * open/create the output file 
+  /*
+   * open/create the output file
    */
   if ((fdout = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) {
     sprintf (buf, "can't create %s for writing", argv[2]);
@@ -54,34 +54,52 @@ int main (int argc, char *argv[])
     exit(errno);
   }
 
-  /* 
-   * 1. find size of input file 
+  /*
+   * 1. find size of input file
+   */
+   struct stat st;
+   off_t fileSize;
+   if (fstat(fdin, &st) == 0)
+   {
+       fileSize = st.st_size;
+
+   }
+   else
+   {
+     printf("Could not determine size of input file");
+   }
+
+  /*
+   * 2. go to the location corresponding to the last byte
+   */
+   if(lseek(fdin, fileSize-1, SEEK_SET) < 0)
+   {
+     printf("lseek error");
+   }
+  /*
+   * 3. write a dummy byte at the last location
+   */
+   size_t size = 1;
+   int err = write(fdin, buf, size);
+   if( err < 0)
+   {
+     printf("write error");
+   }
+
+  /*
+   * 4. mmap the input file
    */
 
-  /* 
-   * 2. go to the location corresponding to the last byte 
+  /*
+   * 5. mmap the output file
    */
 
-  /* 
-   * 3. write a dummy byte at the last location 
-   */
-
-  /* 
-   * 4. mmap the input file 
-   */
-
-  /* 
-   * 5. mmap the output file 
-   */
-
-  /* 
-   * 6. copy the input file to the output file 
+  /*
+   * 6. copy the input file to the output file
    */
     /* Memory can be dereferenced using the * operator in C.  This line
      * stores what is in the memory location pointed to by src into
      * the memory location pointed to by dest.
      */
     *dst = *src;
-} 
-
-
+}
